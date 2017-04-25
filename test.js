7445487fb9d088d6264a57eebf5a8b39640f3a05
ETHAN ANDERSON (CRGL-THIRDPARTY.COM) <ETHAN_ANDERSON@CRGL-THIRDPARTY.COM>
2017-04-25 11:51:40 -0500

    init

---
+++ test.js
@@ -0,0 +1,57 @@
+var TEST = (function() {
+
+  var test = {};
+
+	////////////////////// Support Code //////////////////////
+
+	test.Test = function(args, expected) {
+  	this.args = args;
+    this.expected = expected;
+  };
+
+	test.test = function(func, testArgs) {
+
+    var funcName = func.name;
+
+    var panel = document.createElement('div');
+    panel.classList.add('panel', 'panel-info');
+
+    var panelHeading = document.createElement('div');
+    panelHeading.classList.add('panel-heading');
+    panelHeading.textContent = funcName;
+
+    var panelBody = document.createElement('div');
+    panelBody.classList.add('panel-body');
+
+    var results = document.createElement('ol');
+    results.id = 'results-' + funcName;
+    results.classList.add('list-group');
+
+    panel.appendChild(panelHeading);
+    panel.appendChild(panelBody);
+    panelBody.appendChild(results);
+
+    document.getElementById('results').appendChild(panel);
+
+    for (var i = 0; i < testArgs.length; i++) {
+
+  		var result = func.apply(this, testArgs[i].args);
+    	var li = document.createElement('li');
+      li.classList.add('list-group-item');
+
+      var resultHtml = funcName + '(' + testArgs[i].args.join(', ') + '): ';
+      if (testArgs[i].expected === result) {
+      	resultHtml += '<span class="badge badge-success">' + result + '&nbsp;&nbsp;\u2714</span>';
+      } else {
+        resultHtml += '<span class="badge badge-error">' + result +
+          '&nbsp;&nbsp;\u2718  ( Expected: ' + testArgs[i].expected + ' )</span>';
+      }
+
+      li.innerHTML = resultHtml;
+  		results.appendChild(li);
+		}
+  };
+
+  return test;
+
+}());
---
+++ warmup-1.js
@@ -0,0 +1,45 @@
+(function(Test, test) {
+
+	// Warmup-1 > sleepIn
+
+	var sleepInTests = [
+  	new Test([false, false], true),
+  	new Test([true, false], false),
+  	new Test([false, true], true)
+  ];
+
+	function sleepIn(weekday, vacation) {
+  	return !weekday || vacation;
+	}
+
+  test(sleepIn, sleepInTests);
+
+  // Warmup-1 > monkeyTrouble
+
+  var monkeyTroubleTests = [
+  	new Test([true, true], true),
+    new Test([false, false], true),
+    new Test([true, false], false)
+  ];
+
+  function monkeyTrouble(aSmile, bSmile) {
+  	return aSmile && bSmile || !aSmile && !bSmile;
+  }
+
+  test(monkeyTrouble, monkeyTroubleTests);
+
+  // Warmup-1 > sumDouble
+
+  var sumDoubleTests = [
+  	new Test([1, 2], 3),
+    new Test([3, 2], 5),
+    new Test([2, 2], 8)
+  ];
+
+  function sumDouble(a, b) {
+  	return a === b ? (a + b) * 2 : a + b;
+  }
+
+  test(sumDouble, sumDoubleTests);
+
+})(TEST.Test, TEST.test);
