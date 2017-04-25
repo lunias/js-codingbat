ad9f3f211a35f2a014d88c5579ab2cf4ee50ef11
ETHAN ANDERSON (CRGL-THIRDPARTY.COM) <ETHAN_ANDERSON@CRGL-THIRDPARTY.COM>
2017-04-25 12:36:52 -0500

    untabify 2

--- warmup-1.js
+++ warmup-1.js
@@ -1,29 +1,29 @@
 (function(Test, test) {
 
-	// Warmup-1 > sleepIn
+  // Warmup-1 > sleepIn
 
-	var sleepInTests = [
-  	new Test([false, false], true),
-  	new Test([true, false], false),
-  	new Test([false, true], true)
+  var sleepInTests = [
+    new Test([false, false], true),
+    new Test([true, false], false),
+    new Test([false, true], true)
   ];
 
-	function sleepIn(weekday, vacation) {
-  	return !weekday || vacation;
-	}
+  function sleepIn(weekday, vacation) {
+    return !weekday || vacation;
+  }
 
   test(sleepIn, sleepInTests);
 
   // Warmup-1 > monkeyTrouble
 
   var monkeyTroubleTests = [
-  	new Test([true, true], true),
+    new Test([true, true], true),
     new Test([false, false], true),
     new Test([true, false], false)
   ];
 
   function monkeyTrouble(aSmile, bSmile) {
-  	return aSmile && bSmile || !aSmile && !bSmile;
+    return aSmile && bSmile || !aSmile && !bSmile;
   }
 
   test(monkeyTrouble, monkeyTroubleTests);
@@ -31,13 +31,13 @@
   // Warmup-1 > sumDouble
 
   var sumDoubleTests = [
-  	new Test([1, 2], 3),
+    new Test([1, 2], 3),
     new Test([3, 2], 5),
     new Test([2, 2], 8)
   ];
 
   function sumDouble(a, b) {
-  	return a === b ? (a + b) * 2 : a + b;
+    return a === b ? (a + b) * 2 : a + b;
   }
 
   test(sumDouble, sumDoubleTests);
